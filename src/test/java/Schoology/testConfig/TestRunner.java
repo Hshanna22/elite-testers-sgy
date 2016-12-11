9f2be757ee429f683bf06ca154e8d50cb3fce3f6
Ish1108 <ishmam@mises.com>
2016-12-11 14:04:27 -0500

    Fixed Test Runner

--- src/test/java/Schoology/testConfig/TestRunner.java
+++ src/test/java/Schoology/testConfig/TestRunner.java
@@ -1,5 +1,21 @@
 package Schoology.testConfig;
 
+import org.junit.runner.RunWith;
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+		
+		
+		features = {"Schoology-UserStories"},
+		glue = {"Schoology.stepDefinition"},
+		tags = { "@Test" },
+		format = { "pretty", "html:target" })
+
+
+
+
 public class TestRunner {
 	
 }
